f13094a757c67d7d2371a4d939b931506b017adb
BubbaTing <ronbubbating@hotmail.com>
2019-11-13 16:23:22 -0500

    update the pom.xml files and some class

---
+++ src/main/java/project2/controls/Controller.java
@@ -0,0 +1,10 @@
+package project2.controls;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("events")
+public class Controller {
+
+}
