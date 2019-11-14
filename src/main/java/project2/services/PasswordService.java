2b35b0bf051386cfada70882139645f26a05afcc
ChrisTroll <ctroll333@gmail.com>
2019-11-14 17:07:54 -0500

    Adds PasswordService that generates salt and hash values

---
+++ src/main/java/project2/services/PasswordService.java
@@ -0,0 +1,35 @@
+package project2.services;
+
+import java.security.SecureRandom;
+import java.security.spec.KeySpec;
+
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+
+public class PasswordService {
+	/**
+	 * generates a salt value as a byte[], length 512
+	 */
+	public byte[] genSalt(){
+		SecureRandom random = new SecureRandom();
+		byte[] salt = new byte[512];
+		random.nextBytes(salt);
+		return salt;
+	}
+	
+	/**
+	 * returns a hash value as a byte[] given a salt value length 512 and password string
+	 */
+	public byte[] genHash(byte[] salt, String password){
+		KeySpec spec = new PBEKeySpec(password.toCharArray(), salt, 65536, 512);
+		SecretKeyFactory factory;
+		try {
+			factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA512");
+			byte[] hash = factory.generateSecret(spec).getEncoded();
+			return hash;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+}
