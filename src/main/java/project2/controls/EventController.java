54e75cd16aac70aae632e7fe9952d0c55d54ad50
BubbaTing <ronbubbating@hotmail.com>
2019-11-20 19:23:05 -0500

    add Cross origin to the rest of the controller

--- src/main/java/project2/controls/EventController.java
+++ src/main/java/project2/controls/EventController.java
@@ -2,6 +2,7 @@ package project2.controls;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,7 @@ import project2.entities.Event;
 import project2.services.EventService;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("events/*")
 public class EventController {
 
