60927688604f513b18f6d7004b9eec7c04f5fe9f
BubbaTing <ronbubbating@hotmail.com>
2019-11-23 20:15:36 -0500

    completed user controller test

--- src/main/java/project2/controls/UserController.java
+++ src/main/java/project2/controls/UserController.java
@@ -5,15 +5,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import project2.entities.Attendants;
-import project2.entities.Event;
 import project2.entities.Users;
 import project2.models.UserRegistration;
 import project2.models.UserResponse;
